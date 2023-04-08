b26a626d64816b4ac09cc07af8036af1342fbbeb
George Sedky <george@devopzilla.com>
2023-04-08 12:10:23 +0000

    Update copyright

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -145,7 +145,7 @@ const config = {
                     //     ],
                     // },
                 ],
-                copyright: `Copyright © ${new Date().getFullYear()} Devopzilla. Built with Docusaurus.`,
+                copyright: `Copyright © ${new Date().getFullYear()} Stakpak Inc. Built with Docusaurus.`,
             },
             prism: {
                 theme: lightCodeTheme,
